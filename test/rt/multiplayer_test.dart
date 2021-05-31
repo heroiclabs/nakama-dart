36a17b388543972f79f5d0e4144005ab7169408c
Oliver Brunsmann <oliver.brunsmann@kernwerk.de>
2021-05-31 21:01:20 +0200

    Adds a test to check if two clients can connect and see each other

--- test/rt/multiplayer_test.dart
+++ test/rt/multiplayer_test.dart
@@ -1,5 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:nakama/api/rtapi/realtime.pb.dart';
+import 'package:nakama/api/rtapi/realtime.pbjson.dart';
 import 'package:nakama/nakama.dart';
 import 'package:nakama/src/nakama_websocket_client.dart';
 import 'package:test/test.dart';
@@ -12,11 +13,6 @@ void main() {
 
   // Create a new websocket connection for the hole test run (singleton).
   setUpAll(() async {
-    // Generate test user's credentials.
-    email = faker.internet.freeEmail();
-    password = faker.internet.password();
-    print('User data: $email:$password');
-
     // Create nakama clients.
     final client = NakamaGrpcClient(
       host: kTestHost,
@@ -25,15 +21,29 @@ void main() {
     );
 
     final session = await client.authenticateEmail(
-      email: email,
-      password: password,
+      email: faker.internet.freeEmail(),
+      password: faker.internet.password(),
     );
 
-    // create websocket connetion for lcl test
+    // Create main websocket connetion for lcl test.
     NakamaWebsocketClient.init(
       host: kTestHost,
       ssl: false,
       token: session.token,
+    );
+
+    // Create another websocket connection for another user.
+    final anotherSession = await client.authenticateEmail(
+      email: faker.internet.freeEmail(),
+      password: faker.internet.password(),
+    );
+
+    // Create main websocket connetion for lcl test.
+    NakamaWebsocketClient.init(
+      key: 'clientb',
+      host: kTestHost,
+      ssl: false,
+      token: anotherSession.token,
     );
   });
 
@@ -52,6 +62,20 @@ void main() {
         s.onMatchPresence,
         emits(isA<MatchPresenceEvent>()),
       );
+    });
+
+    test('two clients can join a match', () async {
+      final a = NakamaWebsocketClient.instance;
+      final b = NakamaWebsocketClient.instanceFor(key: 'clientb');
+
+      // Expect to see B joining from A's point of view
+      a.onMatchPresence.listen(expectAsync1((event) {
+        // We first see A then in a next notification we see B joining.
+        expect(event.joins, hasLength(1));
+      }, count: 2));
+
+      // A creates a match, B joins
+      await a.createMatch().then((match) => b.joinMatch(match.matchId));
     });
   });
 }
