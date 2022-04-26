90fba3eb628872419c52cb8bdb017039694f7a47
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-04-26 23:22:50 +0100

    rest interface

---
+++ lib/rest.dart
@@ -0,0 +1,7 @@
+library rest;
+
+export './src/enum/storage_permission.dart';
+export './src/nakama_client/nakama_client.dart';
+export './src/nakama_client/nakama_client_stub.dart';
+export './src/nakama_websocket_client.dart';
+export './src/session.dart';
