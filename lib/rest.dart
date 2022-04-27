724fc83f59506cc560fe87443591fd9e1e8806f4
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-04-27 09:38:12 +0100

    edit rest library

--- lib/rest.dart
+++ lib/rest.dart
@@ -1,7 +1,8 @@
 library rest;
 
-export './src/enum/storage_permission.dart';
-export './src/nakama_client/nakama_client.dart';
-export './src/nakama_client/nakama_client_stub.dart';
-export './src/nakama_websocket_client.dart';
+// export './src/enum/storage_permission.dart';
+// export './src/nakama_client/nakama_client.dart';
+export './src/nakama_client/nakama_grpc_client.dart';
+// export './src/nakama_client/nakama_client_stub.dart';
+// export './src/nakama_websocket_client.dart';
 export './src/session.dart';
