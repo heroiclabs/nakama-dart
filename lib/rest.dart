13c9ac0fcb54b7428153d86bcc1a1648d1743ecf
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-04-29 09:06:24 +0100

    rest api client

--- lib/rest.dart
+++ lib/rest.dart
@@ -3,6 +3,7 @@ library rest;
 export 'package:fixnum/fixnum.dart' show Int64;
 
 export './src/enum/storage_permission.dart';
+export './src/nakama_client/nakama_api_client.dart'; // rest api client
 export './src/nakama_client/nakama_client.dart';
 // export './src/nakama_client/nakama_client_stub.dart'; get nakama client from rest
 export './src/nakama_websocket_client.dart';
