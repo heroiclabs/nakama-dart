060a2525e76dd9393a655fda5ab0c59802fdfab6
Fayyad <fayyadsufyan@gmail.com>
2024-03-15 22:29:14 +0400

    Add satori client tests.

---
+++ lib/satori/test/client_test.dart
@@ -0,0 +1,75 @@
+import 'package:satori/satori.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('[REST] Test Satori', () {
+    late final SatoriRestApiClient client;
+
+    setUpAll(() async {
+      client = SatoriRestApiClient.init(
+        host: '127.0.0.1',
+        port: 7450,
+        apiKey: 'apikey',
+        ssl: false,
+      );
+    });
+
+    test('authenticate and logout', () async {
+      // Authenticate
+      final session = await client.authenticate(id: '285fb548-1c23-42c2-84b5-cd18c22d7053');
+
+      expect(session, isA<Session>());
+
+      // Logout
+      await client.authenticateLogout(session: session);
+      expect(() async => await client.getExperiments(session: session, names: []), throwsA(isA<Exception>()));
+    });
+
+    test('get experiments', () async {
+      final session = await client.authenticate(id: '285fb548-1c23-42c2-84b5-cd18c22d7053');
+      final experiments = await client.getAllExperiments(session: session);
+
+      expect(experiments, isA<ExperimentList>());
+      expect(experiments.experiments, isNotNull);
+      expect(experiments.experiments.length, equals(1));
+    });
+
+    test('get flags', () async {
+      final session = await client.authenticate(id: '285fb548-1c23-42c2-84b5-cd18c22d7053');
+      final flags = await client.getFlags(session: session, names: []);
+
+      expect(flags, isA<FlagList>());
+      expect(flags.flags, isNotNull);
+      expect(flags.flags.length, equals(4));
+    });
+
+    test('test send events', () async {
+      final session = await client.authenticate(id: '285fb548-1c23-42c2-84b5-cd18c22d7053');
+      await client.event(
+        session: session,
+        event: Event(
+          name: 'gameFinished',
+          timestamp: DateTime.now(),
+          metadata: {'score': '100'},
+        ),
+      );
+
+      await client.events(
+        session: session,
+        events: [
+          Event(name: 'appLaunched', timestamp: DateTime.now()),
+          Event(name: 'adStarted', timestamp: DateTime.now()),
+        ],
+      );
+    });
+
+    test('test get live events', () async {
+      final session = await client.authenticate(id: '285fb548-1c23-42c2-84b5-cd18c22d7053');
+      final liveEvents = await client.getLiveEvents(session: session);
+
+      // Should not receive any live events because not in the targeted audience
+      expect(liveEvents, isA<LiveEventList>());
+      expect(liveEvents.liveEvents, isEmpty);
+    });
+  });
+}
---
+++ lib/satori/test/identify_test.dart
@@ -0,0 +1,86 @@
+import 'package:satori/satori.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('[REST] Test Satori Identify', () {
+    late final SatoriRestApiClient client;
+
+    setUpAll(() async {
+      client = SatoriRestApiClient.init();
+    });
+
+    test('identify tests', () async {
+      final session1 = await client.authenticate(id: '11111111-1111-0000-0000-000000000000');
+
+      final props1 = {'email': 'a@b.com', 'pushTokenIos': 'foo'};
+      final customProps1 = {'earlyAccess': 'true'};
+
+      await client.updateProperties(
+        session: session1,
+        defaultProperties: props1,
+        customProperties: customProps1,
+      );
+
+      final events = [
+        Event(name: 'awardReceived', timestamp: DateTime.now()),
+        Event(name: 'inventoryUpdated', timestamp: DateTime.now()),
+      ];
+      await client.events(session: session1, events: events);
+
+      // Sleep for 2 seconds
+      await Future.delayed(const Duration(seconds: 2));
+
+      final session2 = await client.authenticate(id: '22222222-2222-0000-0000-000000000000');
+
+      final props2 = {'email': 'a@b.com', 'pushTokenAndroid': 'bar'};
+      final customProps2 = {'earlyAccess': 'false'};
+
+      await client.updateProperties(
+        session: session2,
+        defaultProperties: props2,
+        customProperties: customProps2,
+      );
+
+      await Future.delayed(const Duration(seconds: 2));
+
+      final session = await client.identify(
+        session: session1,
+        id: '22222222-2222-0000-0000-000000000000',
+        defaultProperties: {},
+        customProperties: {},
+      );
+
+      expect(session, isNotNull);
+      expect(session.identityId, '22222222-2222-0000-0000-000000000000');
+
+      final props = await client.listProperties(session: session);
+
+      expect(props.$default?.length, greaterThan(0));
+      expect(props.custom?.length, greaterThan(0));
+
+      // Default properties
+      expect(props.$default, isNotNull);
+      expect(props.$default, isNotEmpty);
+      for (final entry in props.$default!.entries) {
+        switch (entry.key) {
+          case 'email':
+            expect(entry.value, 'a@b.com');
+          case 'pushTokenAndroid':
+            expect(entry.value, 'bar');
+          case 'pushTokenIos':
+            expect(entry.value, 'foo');
+        }
+      }
+
+      // Custom properties
+      expect(props.custom, isNotNull);
+      expect(props.custom, isNotEmpty);
+      for (final entry in props.custom!.entries) {
+        switch (entry.key) {
+          case 'earlyAccess':
+            expect(entry.value, 'false');
+        }
+      }
+    });
+  });
+}
