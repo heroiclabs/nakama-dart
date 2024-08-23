daf84accd7a6c5dbb444919591012b6c925361ab
Gabriel Terwesten <gabriel@terwesten.net>
2024-08-23 19:07:44 +0200

    Control the public api with `show` (#13)

--- nakama/lib/nakama.dart
+++ nakama/lib/nakama.dart
@@ -1,25 +1,43 @@
 library nakama;
 
-export 'package:fixnum/fixnum.dart' show Int64;
-
-export 'src/client.dart';
-export 'src/models/account.dart';
-export 'src/models/channel_message.dart';
-export 'src/models/channel_type.dart';
-export 'src/models/chat.dart';
-export 'src/models/friends.dart';
-export 'src/models/friendship_state.dart';
-export 'src/models/group.dart';
-export 'src/models/group_membership_states.dart';
-export 'src/models/leaderboard.dart';
-export 'src/models/match.dart';
-export 'src/models/matchmaker.dart';
-export 'src/models/notification.dart';
-export 'src/models/party.dart';
-export 'src/models/rpc.dart';
-export 'src/models/session.dart';
-export 'src/models/status.dart';
-export 'src/models/storage.dart';
-export 'src/models/storage_permission.dart';
-export 'src/models/tournament.dart';
-export 'src/socket.dart';
+export 'src/client.dart' show Client;
+export 'src/models/account.dart' show Account, Device, User;
+export 'src/models/channel_message.dart'
+    show ChannelMessage, ChannelMessageList;
+export 'src/models/channel_type.dart' show ChannelType;
+export 'src/models/chat.dart' show Channel, ChannelMessageAck;
+export 'src/models/friends.dart' show Friend, FriendsList;
+export 'src/models/friendship_state.dart' show FriendshipState;
+export 'src/models/group.dart'
+    show Group, GroupList, GroupUser, GroupUserList, UserGroup, UserGroupList;
+export 'src/models/group_membership_states.dart' show GroupMembershipState;
+export 'src/models/leaderboard.dart'
+    show LeaderboardRecord, LeaderboardRecordList, LeaderboardOperator;
+export 'src/models/match.dart' show Match, Party, RealtimeMatch;
+export 'src/models/matchmaker.dart'
+    show
+        ChannelPresenceEvent,
+        MatchData,
+        MatchPresenceEvent,
+        MatchmakerMatched,
+        MatchmakerTicket,
+        MatchmakerUser,
+        PartyMatchmakerTicket;
+export 'src/models/notification.dart' show Notification, NotificationList;
+export 'src/models/party.dart' show PartyData, PartyLeader, PartyPresenceEvent;
+export 'src/models/rpc.dart' show Rpc;
+export 'src/models/session.dart' show Session;
+export 'src/models/status.dart'
+    show
+        RealtimeStream,
+        RealtimeStreamData,
+        StatusPresenceEvent,
+        StreamPresenceEvent,
+        UserPresence;
+export 'src/models/storage.dart'
+    show StorageObject, StorageObjectId, StorageObjectList, StorageObjectWrite;
+export 'src/models/storage_permission.dart'
+    show StorageReadPermission, StorageWritePermission;
+export 'src/models/tournament.dart'
+    show Tournament, TournamentList, TournamentRecordList;
+export 'src/socket.dart' show Socket;
--- satori/lib/satori.dart
+++ satori/lib/satori.dart
@@ -1,14 +1,13 @@
 library satori;
 
-// Models
-export 'src/models/event.dart';
-export 'src/models/experiment.dart';
-export 'src/models/flag.dart';
-export 'src/models/live_event.dart';
-export 'src/models/properties.dart';
-export 'src/models/session.dart';
-// Client
-export 'src/satori_client/satori_api_client.dart';
-export 'src/satori_client/satori_client.dart';
+export 'src/models/event.dart' show Event;
+export 'src/models/experiment.dart' show Experiment, ExperimentList;
+export 'src/models/flag.dart' show Flag, FlagList;
+export 'src/models/live_event.dart' show LiveEvent, LiveEventList;
+export 'src/models/properties.dart' show Properties;
+export 'src/models/session.dart' show Session;
+export 'src/satori_client/satori_api_client.dart' show SatoriRestApiClient;
+export 'src/satori_client/satori_client.dart' show SatoriBaseClient;
 export 'src/satori_client/stub/satori_client_stub.dart'
-    if (dart.library.js) 'src/satori_client/stub/api_client.dart';
+    if (dart.library.js) 'src/satori_client/stub/api_client.dart'
+    show getSatoriClient;
