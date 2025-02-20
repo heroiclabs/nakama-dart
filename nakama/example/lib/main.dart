3efd14d0e758f2f93bf67fd19638c89b66fa26ee
ilmalte <17677294+ilmalte@users.noreply.github.com>
2025-02-20 18:53:11 +0100

    Add leaderboards example

--- nakama/example/lib/main.dart
+++ nakama/example/lib/main.dart
@@ -5,6 +5,8 @@ import 'package:nakama_example/features/authentication/login/views/pages/login_p
 import 'package:nakama_example/features/authentication/signup/views/pages/signup_page.dart';
 import 'package:nakama_example/features/common/routes.dart';
 import 'package:nakama_example/features/home/list_features/views/pages/list_features.dart';
+import 'package:nakama_example/features/leaderboard/views/pages/leaderboard.dart';
+import 'package:nakama_example/features/leaderboard/views/pages/leaderboards.dart';
 import 'package:nakama_example/features/rpc/custom/views/pages/rpc_custom.dart';
 
 void main() {
@@ -20,6 +22,19 @@ void main() {
         Routes.signup: (context) => SignupPage(),
         Routes.home: (context) => ListFeaturesPage(),
         Routes.rpc: (context) => RpcCustomPage(),
+        Routes.leaderboards: (context) => LeaderboardsPage(),
+      },
+      onGenerateRoute: (settings) {
+        // Routes.leaderboard
+        if (settings.name != null &&
+            settings.name!.startsWith(Routes.leaderboards)) {
+          final id = settings.name!.replaceFirst('${Routes.leaderboards}/', '');
+          return MaterialPageRoute(
+            builder: (context) => LeaderboardPage(),
+            settings: RouteSettings(arguments: id),
+          );
+        }
+        return null;
       },
     ),
   ));
