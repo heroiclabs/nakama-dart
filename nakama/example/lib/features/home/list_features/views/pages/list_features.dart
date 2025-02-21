913c7cef2ce1da8491cef5d761e1db65bba53f1e
ilmalte <17677294+ilmalte@users.noreply.github.com>
2025-02-21 15:54:12 +0100

    Add groups example

--- nakama/example/lib/features/home/list_features/views/pages/list_features.dart
+++ nakama/example/lib/features/home/list_features/views/pages/list_features.dart
@@ -20,6 +20,24 @@ class ListFeaturesPage extends ConsumerWidget {
                     onPressed: () async {
                       try {
                         if (context.mounted) {
+                          await Navigator.pushNamed(context, Routes.groups);
+                        }
+                      } catch (e) {
+                        if (context.mounted) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(e.toString()),
+                          ));
+                        }
+                      }
+                    },
+                    child: const Text('Create, list and update groups')),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                    onPressed: () async {
+                      try {
+                        if (context.mounted) {
                           await Navigator.pushNamed(context, Routes.rpc);
                         }
                       } catch (e) {
