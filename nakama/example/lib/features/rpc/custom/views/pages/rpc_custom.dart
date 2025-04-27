818cecce110563d3d68c3696f6a5c2719310996e
Daniele Maltese <17677294+ilmalte@users.noreply.github.com>
2025-04-27 23:31:24 +0200

    fix: RPC payload encoding for Web #107

--- nakama/example/lib/features/rpc/custom/views/pages/rpc_custom.dart
+++ nakama/example/lib/features/rpc/custom/views/pages/rpc_custom.dart
@@ -47,11 +47,37 @@ class RpcCustomPage extends ConsumerWidget {
                   onPressed: () async => _callRpc(
                     context,
                     ref,
+                    () => ref.read(rpcCustomProvider.notifier).callWithPayload(
+                        'hello_world', {'name': 'HTTP with payload'}),
+                  ),
+                  child: const Text('Call "hello_world" (HTTP) with payload'),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () async => _callRpc(
+                    context,
+                    ref,
                     () => ref
                         .read(rpcCustomProvider.notifier)
                         .callWS('hello_world'),
                   ),
                   child: const Text('Call "hello_world (WebSocket)"'),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () async => _callRpc(
+                    context,
+                    ref,
+                    () => ref
+                        .read(rpcCustomProvider.notifier)
+                        .callWSWithPayload(
+                            'hello_world', {'name': 'Websocket with payload'}),
+                  ),
+                  child: const Text('Call "hello_world (WebSocket)" with payload'),
                 ),
               ),
             ],
