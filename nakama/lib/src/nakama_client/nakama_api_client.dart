62634ae0df9190eca10304d98ceb889f46eb6026
jakeobrien <jakeobrien@users.noreply.github.com>
2025-06-20 16:38:28 -0400

    Nulling out session in authenticateCustom fixing bug where it fails with session

--- nakama/lib/src/nakama_client/nakama_api_client.dart
+++ nakama/lib/src/nakama_client/nakama_api_client.dart
@@ -654,6 +654,7 @@ class NakamaRestApiClient extends NakamaBaseClient {
     String? username,
     Map<String, String>? vars,
   }) async {
+    _session = null;
     try {
       final session = await _api.authenticateCustom(
         body: ApiAccountCustom(id: id, vars: vars),
